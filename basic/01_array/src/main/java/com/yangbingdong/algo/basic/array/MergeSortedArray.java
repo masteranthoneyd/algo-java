9e44f6efab4627b7eea4b5716bf3b99cec3ee262
yangbingdong <yangbingdong1994@gmail.com>
2021-05-07 18:02:25 +0800

    add leetcode 88 solution

--- basic/01_array/src/main/java/com/yangbingdong/algo/basic/array/MergeSortedArray.java
+++ basic/01_array/src/main/java/com/yangbingdong/algo/basic/array/MergeSortedArray.java
@@ -7,7 +7,64 @@ package com.yangbingdong.algo.basic.array;
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        mergeInner2(nums1, m, nums2, n);
     }
+
+    /**
+     * 新建一个长度为 m+n 的数组
+     * 逐个对比 nums1 与 nums2 元素, 小的元素追加到新的数组中
+     * 最后再将新的数组赋值给 nums1
+     */
+    private void mergeInner1(int[] nums1, int m, int[] nums2, int n) {
+        int[] ret = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (j < m && k < n) {
+            if (nums1[j] < nums2[k]) {
+                ret[i++] = nums1[j++];
+            } else {
+                ret[i++] = nums2[k++];
+            }
+        }
+
+        if (j < m) {
+            while (j < m) {
+                ret[i++] = nums1[j++];
+            }
+        }
+        if (k < n) {
+            while (k < n) {
+                ret[i++] = nums2[k++];
+            }
+        }
+        System.arraycopy(ret, 0, nums1, 0, ret.length);
+    }
+
+    /**
+     * 不消耗外空间
+     * 由于两个数组已排好序, 所以可以采取从后向前填充最大值的方式解决
+     */
+    private void mergeInner2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        int j = m - 1, k = n - 1;
+        while (j >= 0 && k >= 0) {
+            if (nums1[j] > nums2[k]) {
+                nums1[i--] = nums1[j--];
+            }else {
+                nums1[i--] = nums2[k--];
+            }
+        }
+        if (j >= 0) {
+            while (j >= 0) {
+                nums1[i--] = nums1[j--];
+            }
+        }
+
+        if (k >= 0) {
+            while (k >= 0) {
+                nums1[i--] = nums2[k--];
+            }
+        }
+    }
+
 
 }
--- basic/01_array/src/test/java/com/yangbingdong/algo/basic/array/MergeSortedArrayTest.java
+++ basic/01_array/src/test/java/com/yangbingdong/algo/basic/array/MergeSortedArrayTest.java
@@ -3,6 +3,8 @@ package com.yangbingdong.algo.basic.array;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author <a href="mailto:yangbingdong1994@gmail.com">yangbingdong</a>
  * @since
@@ -17,6 +19,67 @@ class MergeSortedArrayTest {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
         mergeSortedArray.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
         Assertions.assertArrayEquals(nums1, new int[]{1, 2, 2, 3, 5, 6});
+    }
+
+    @Test
+    void testMerge2() {
+        // nums1 = [1], m = 1, nums2 = [], n = 0
+        // [1]
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1};
+        int[] nums2 = {};
+        mergeSortedArray.merge(nums1, 1, nums2, 0);
+        System.out.println(Arrays.toString(nums1));
+        Assertions.assertArrayEquals(nums1, new int[]{1});
+    }
+
+    @Test
+    void testMerge3() {
+        // nums1 = [0], m = 0, nums2 = [1], n = 1
+        // [1]
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        mergeSortedArray.merge(nums1, 0, nums2, 1);
+        System.out.println(Arrays.toString(nums1));
+        Assertions.assertArrayEquals(nums1, new int[]{1});
+    }
+
+    @Test
+    void testMerge4() {
+        // nums1 = [2, 0], m = 1, nums2 = [1], n = 1
+        // [1]
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {2, 0};
+        int[] nums2 = {1};
+        mergeSortedArray.merge(nums1, 1, nums2, 1);
+        System.out.println(Arrays.toString(nums1));
+        Assertions.assertArrayEquals(nums1, new int[]{1, 2});
+    }
+
+    @Test
+    void testMerge5() {
+        // nums1 = [1,2,3,0,0,0], m = 3, nums2 = [4,5,6], n = 3
+        // [1]
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {4, 5, 6};
+        mergeSortedArray.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+        Assertions.assertArrayEquals(nums1, new int[]{1, 2, 3, 4, 5, 6});
+    }
+
+    @Test
+    void testMerge6() {
+        // nums1 = [0,0,3,0,0,0,0,0,0], m = 3, nums = [-1,1,1,1,2,3], n = 6
+        // [-1,0,0,1,1,1,2,3,3]
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {0, 0, 3, 0, 0, 0, 0, 0, 0};
+        int[] nums2 = {-1, 1, 1, 1, 2, 3};
+        mergeSortedArray.merge(nums1, 3, nums2, 6);
+        System.out.println(Arrays.toString(nums1));
+        Assertions.assertArrayEquals(nums1, new int[]{-1, 0, 0, 1, 1, 1, 2, 3, 3});
     }
 }
